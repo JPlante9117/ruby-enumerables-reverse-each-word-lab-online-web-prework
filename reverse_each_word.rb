d54e2b62b0f7abec4a9dd864810f4dd1ec6b01e9
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-06 18:16:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,6 @@
-def reverse_each_word(sentence)
+def reverse_each_word
   
-  sentence_array = sentence.split(" ")
   
-  reversed_array = sentence_array.each{|string| string.reverse!}
-  
-  final_string = reversed_array.join(' ')
-  
-  p final_string
   
 end
 
